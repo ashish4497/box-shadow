3a5be590364a77db40de359bcbf7307241134662
ashish497 <ak839326@gmail.com>
2020-07-02 11:22:03 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [Hori, setHori] = useState(10);
+  const [Veri, setVeri] = useState(10);
+  const [Blur, setBlur] = useState(10);
+  const [Color, setColor] = useState("black");
+  const [check, setCheck] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>box-shadow generator</h4>
+      <div className="parent">
+        <div className="controls">
+          <label>Horizontal length</label>
+          <input
+            type="range"
+            min="-200"
+            max="200"
+            value={Hori}
+            onChange={(e) => setHori(e.target.value)}
+          />
+
+          <label>Vertical length</label>
+          <input
+            type="range"
+            min="-200"
+            max="200"
+            value={Veri}
+            onChange={(e) => setVeri(e.target.value)}
+          />
+          <label>Blur</label>
+          <input
+            type="range"
+            min="0"
+            max="200"
+            value={Blur}
+            onChange={(e) => setBlur(e.target.value)}
+          />
+          <label>Color</label>
+          <input
+            type="color"
+            value={Color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <div className="switch">
+            <label>
+              Outline
+              <input
+                type="checkbox"
+                checked={check}
+                onChange={() => setCheck(!check)}
+              />
+              <span className="lever"></span>
+              Inset
+            </label>
+          </div>
+        </div>
+        <div className="output">
+          <div
+            className="box"
+            style={{
+              boxShadow: `${check ? "Inset" : ""} 
+              ${Hori}px ${Veri}px ${Blur}px ${Color}`,
+            }}
+          >
+            <p>
+              box-shadow: {Hori}px {Veri}px {Blur}px {Color} ;
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
